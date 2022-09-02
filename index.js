ca9bbfc300ec1280ba98084b4ac77b1b49443dfa
FeLonCar1107 <pipe20001107@gmail.com>
2022-09-02 16:25:21 -0500

    conexion con el cluster

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require("express")
-const mongoose = require("mongoose")
-
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express()
-const port = 3000
+const port = 4000
+
+mongoose.connect("mongodb+srv://electiva1:electiva1@cluster0.dolot0f.mongodb.net/?retryWrites=true&w=majority"
+).then(() => 
+    console.log("Database conection succesfull")
+).catch(() => 
+    console.log("Database conection error"));
 
 app.listen(port, () =>{console.log('Conect with the port ', port);})
